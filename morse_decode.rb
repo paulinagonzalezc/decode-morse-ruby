50342c136c2b18dac7fc4f0c99502041f85fa15f
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-01-25 14:13:23 -0600

    Start rb file with methods

---
+++ morse_decode.rb
@@ -0,0 +1,25 @@
+# Create a method to decode a Morse code character
+def decode_char(morse)
+  morse_dict = {
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    
+
+  }
+  puts morse_dict[morse]
+end
+
+decode_char(".-")
